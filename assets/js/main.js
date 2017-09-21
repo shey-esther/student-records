0827b8d060881bc92718232bb86ee2befd6ea712
esther <estheralvaro44@gmail.com>
2017-09-20 23:17:51 -0500

    funcion qu imprime los porcentajes mayores a 70

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,20 +2,26 @@
 var arrayDatos = [];//retorna los datos que son ingresados.
 console.log(arrayDatos);
 
+function productos (nombre,porcentaje,hse){
+  this.nombre=nombre,
+  this.porcentaje=porcentaje,
+  this.hse=hse
+  };
 
 $('#students').click(function () {//al hacer click obtenemos datos con el prompt
   var nombre = prompt("Dime tu Nombre");
   var porcentaje = prompt("Punto Tecnico");
   var hse = prompt("Puntos de vida");
-  var productos = [//variable objeto literal.
-    {
-      nombre : nombre,
-      porcentaje : porcentaje,
-      hse : hse
-    },
-  ];
-  for (var i = 0; i < productos.length; i++) {
-      arrayDatos.push(productos[i]);//pusheamos los resultados al array vacio.
+  var nuevoDato = new productos (nombre,porcentaje,hse);//pusheamos los resultados al array vacio.
+  arrayDatos.push(nuevoDato);
+  for (var i = 0; i < arrayDatos.length; i++) {
+    $('#mostrar').empty();
+    $('#mostrar').append(
+      `<h5>Nombre :${arrayDatos[i].nombre}</h5>\
+        <h5>Puntaje Tecnico :${arrayDatos[i].porcentaje}</h5>\
+        <h5>HSE :${arrayDatos[i].hse}</h5>\
+        <hr>
+    `);
   }
 });
 
@@ -35,11 +41,33 @@ function imprimir (array){
 
 
 $('#porcentBajos').click( function (){
- var resslt = arrayDatos.filter(function (menores) {
-  return menores.porcentaje >= 80;
+  $('#mostrar').empty();
+  var resslt = arrayDatos.filter(function (menores) {
+  return menores.porcentaje > 70;
   });
+  imprimir(resslt);
   console.log(resslt);
 });
+
+
+
+$('#btn_update').click(function(){
+  $("#data1").empty();
+  var update=arr_students.filter(function (c) {
+  return c.TechPoints >70
+});
+Print(update);
+console.log(update);
+});
+
+
+
+// $('#porcentBajos').click( function (){
+//   var resslt = arrayDatos.filter(function (menores) {
+//    return menores.porcentaje >= 80;
+//    });
+//    console.log(resslt);
+//  });
 
 
 
